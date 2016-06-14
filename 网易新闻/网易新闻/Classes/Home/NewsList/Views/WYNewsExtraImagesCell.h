69190d59c360952941c7a5ab6e0033df524ef97b
刀哥 <@qq.com>
2016-06-14 17:43:24 +0800

    添加多图的 cell 布局

---
+++ 网易新闻/网易新闻/Classes/Home/NewsList/Views/WYNewsExtraImagesCell.h
@@ -0,0 +1,13 @@
+//
+//  WYNewsExtraImagesCell.h
+//  网易新闻
+//
+//  Created by apple on 16/6/14.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYNewsExtraImagesCell : UITableViewCell
+
+@end
