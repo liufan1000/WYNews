9db19485969c28030cf99ac07482182e889f962a
刀哥 <@qq.com>
2016-06-14 15:52:48 +0800

    添加所有子控制器

--- 网易新闻/网易新闻/Classes/Main/WYMainViewController.m
+++ 网易新闻/网易新闻/Classes/Main/WYMainViewController.m
@@ -26,9 +26,16 @@
  */
 - (void)addChildViewControllers {
     
+    // 设置 tabBar 的字体颜色
+    self.tabBar.tintColor = [UIColor cz_colorWithHex:0xDF0000];
+    
     // 视图控制器的字典数组
     NSArray *array = @[
-                       @{@"clsName": @"UIViewController", @"title": @"首页", @"imageName": @"tabbar_icon_news_normal"},
+                       @{@"clsName": @"UIViewController", @"title": @"新闻", @"imageName": @"news"},
+                       @{@"clsName": @"UIViewController", @"title": @"阅读", @"imageName": @"reader"},
+                       @{@"clsName": @"UIViewController", @"title": @"视频", @"imageName": @"media"},
+                       @{@"clsName": @"UIViewController", @"title": @"话题", @"imageName": @"bar"},
+                       @{@"clsName": @"UIViewController", @"title": @"我", @"imageName": @"me"},
                        ];
     
     // 遍历数组，创建控制器数组
@@ -58,8 +65,12 @@
     vc.title = dict[@"title"];
     
     // 3. 图片
-    NSString *imageName = dict[@"imageName"];
+    NSString *imageName = [NSString stringWithFormat:@"tabbar_icon_%@_normal", dict[@"imageName"]];
     vc.tabBarItem.image = [UIImage imageNamed:imageName];
+    
+    // 高亮图片
+    NSString *imageNameHL = [NSString stringWithFormat:@"tabbar_icon_%@_highlight", dict[@"imageName"]];
+    vc.tabBarItem.selectedImage = [[UIImage imageNamed:imageNameHL] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     
     // 4. 添加导航控制器
     UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:vc];
