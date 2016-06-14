13819474061a62268a4e91804a51d39abe075d9d
刀哥 <@qq.com>
2016-06-14 16:47:43 +0800

    准备新闻接口的网络方法

--- 网易新闻/网易新闻/Tools/Network/CZNetworkManager.h
+++ 网易新闻/网易新闻/Tools/Network/CZNetworkManager.h
@@ -12,4 +12,13 @@
 
 + (instancetype)sharedManager;
 
+/**
+ * 加载网易新闻列表
+ *
+ * @param channel    频道字符串
+ * @param start      开始数字
+ * @param completion 完成回调[字典数组／错误]
+ */
+- (void)newsListWithChannel:(NSString *)channel start:(NSInteger)start completion:(void (^)(NSArray *array, NSError *error))completion;
+
 @end
--- 网易新闻/网易新闻/Tools/Network/CZNetworkManager.m
+++ 网易新闻/网易新闻/Tools/Network/CZNetworkManager.m
@@ -25,4 +25,36 @@
     return instance;
 }
 
+#pragma mark - 封装 AFN 网络请求
+/**
+ * 发起 GET 请求
+ *
+ * @param URLString  URLString
+ * @param parameters 参数字典
+ * @param completion 完成回调[json(字典／数组)，错误]
+ */
+- (void)GETRequest:(NSString *)URLString parameters:(NSDictionary *)parameters completion:(void (^)(id json, NSError *error))completion {
+    
+    [self GET:URLString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        completion(responseObject, nil);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        NSLog(@"网络请求失败 %@", error);
+        
+        completion(nil, error);
+    }];
+}
+
+#pragma mark - 网易新闻接口
+- (void)newsListWithChannel:(NSString *)channel start:(NSInteger)start completion:(void (^)(NSArray *, NSError *))completion {
+    
+    NSString *urlString = [NSString stringWithFormat:@"list/%@/%zd-20.html", channel, start];
+    
+    [self GETRequest:urlString parameters:nil completion:^(id json, NSError *error) {
+        NSLog(@"%@", json);
+    }];
+}
+
 @end
