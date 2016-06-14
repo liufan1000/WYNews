ab8f5ac23c1be67845136c3aa8985941bc4cb37b
刀哥 <@qq.com>
2016-06-14 16:16:12 +0800

    添加表格视图，绑定假数据

--- 网易新闻/网易新闻/Classes/Home/NewsList/WYNewsListViewController.m
+++ 网易新闻/网易新闻/Classes/Home/NewsList/WYNewsListViewController.m
@@ -8,15 +8,18 @@
 
 #import "WYNewsListViewController.h"
 
-@interface WYNewsListViewController ()
+static NSString *cellId = @"cellId";
 
+@interface WYNewsListViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (nonatomic, weak) UITableView *tableView;
 @end
 
 @implementation WYNewsListViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setupUI];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +27,41 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId forIndexPath:indexPath];
+    
+    cell.textLabel.text = @(indexPath.row).description;
+    
+    return cell;
+}
+
+#pragma mark - 设置界面
+- (void)setupUI {
+    
+    UITableView *tv = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+    
+    [self.view addSubview:tv];
+    
+    // 自动布局
+    [tv mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    // 注册原型 cell
+    [tv registerClass:[UITableViewCell class] forCellReuseIdentifier:cellId];
+    
+    // 设置数据源和代理
+    tv.dataSource = self;
+    tv.delegate = self;
+    
+    // 记录成员变量
+    _tableView = tv;
+}
 
 @end
