a37e777ccc23a9f06956c2a0db2dea2efca0aac4
刀哥 <@qq.com>
2016-06-16 11:33:08 +0800

    频道视图中的 label 的布局

--- 网易新闻/网易新闻/Classes/Home/Views/WYChannelView.m
+++ 网易新闻/网易新闻/Classes/Home/Views/WYChannelView.m
@@ -29,14 +29,33 @@
 - (void)setChannelList:(NSArray<WYChannel *> *)channelList {
     _channelList = channelList;
     
+    // 1. 初始数据准备
+    CGFloat x = 20;
+    CGFloat margin = 8;
+    CGFloat height = _scrollView.bounds.size.height;
+    
     // 向 scrollView 添加控件
     for (WYChannel *channel in channelList) {
-        // 1. 创建标签
+        
+        // 1. 创建标签 - 已经计算好合适的 label 的宽度
         WYChannelLabel *l = [WYChannelLabel channelLabelWithTitle:channel.tname];
         
-        // 2. 添加到滚动视图
+        // 2. 设置 label 的位置
+        l.frame = CGRectMake(x, 0, l.bounds.size.width, height);
+        
+        // 3. 递增 x
+        x += l.bounds.size.width + margin;
+        
+        // 4. 添加到滚动视图
         [_scrollView addSubview:l];
     }
+    
+    // 设置 scrollView 的 contentSize
+    _scrollView.contentSize = CGSizeMake(x, height);
+    
+    // 取消滚动指示器
+    _scrollView.showsVerticalScrollIndicator = NO;
+    _scrollView.showsHorizontalScrollIndicator = NO;
 }
 
 @end
