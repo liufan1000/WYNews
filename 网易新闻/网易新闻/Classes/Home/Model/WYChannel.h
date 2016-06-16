ee151c9394dcb5ad5a89ae1c8270c0f1dd408716
刀哥 <@qq.com>
2016-06-16 10:32:30 +0800

    添加新闻频道模型

---
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannel.h
@@ -0,0 +1,22 @@
+//
+//  WYChannel.h
+//  网易新闻
+//
+//  Created by apple on 16/6/16.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WYChannel : NSObject
+
+/**
+ * 频道名称
+ */
+@property (nonatomic, copy) NSString *tname;
+/**
+ * 频道 ID
+ */
+@property (nonatomic, copy) NSString *tid;
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Home/Model/WYChannel.m
@@ -0,0 +1,13 @@
+//
+//  WYChannel.m
+//  网易新闻
+//
+//  Created by apple on 16/6/16.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "WYChannel.h"
+
+@implementation WYChannel
+
+@end
