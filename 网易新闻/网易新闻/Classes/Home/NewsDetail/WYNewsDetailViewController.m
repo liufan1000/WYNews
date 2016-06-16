0392d84bb3bdbf5353a89834cc9c8226cdc84faf
刀哥 <@qq.com>
2016-06-16 19:53:00 +0800

    遍历 img 数组，替换图片

--- 网易新闻/网易新闻/Classes/Home/NewsDetail/WYNewsDetailViewController.m
+++ 网易新闻/网易新闻/Classes/Home/NewsDetail/WYNewsDetailViewController.m
@@ -44,6 +44,29 @@
         NSLog(@"%@", img);
         NSLog(@"%@", video);
         
+        // 追加图片测试数据
+//        body = [body stringByAppendingString:@"<img src=\"http://dingyue.nosdn.127.net/ooo2sfnXhSUz8RYoWesGpNrhDkMaNzdbW1j8ynKDHuIlJ1466045195810.jpg\" />"];
+        
+        // 循环遍历 img 的数组
+        // 查找 body 中 ref 的位置，如果找到使用 src 替换对应的图片内容
+        for (NSDictionary *dict in img) {
+            
+            // 1> 获取 ref 的内容
+            NSString *ref = dict[@"ref"];
+            
+            // 2> 在 body 中查找 ref 的位置
+            NSRange range = [body rangeOfString:ref];
+            
+            // 3> 判断是否找到
+            if (range.location == NSNotFound) {
+                continue;
+            }
+            
+            // 4> 替换 body range 对应的内容
+            NSString *imgStr = [NSString stringWithFormat:@"<img src=\"%@\" />", dict[@"src"]];
+            body = [body stringByReplacingCharactersInRange:range withString:imgStr];
+        }
+        
         // 4. 显示页面
         [self.webView loadHTMLString:body baseURL:nil];
     }];
